8867d1f8d96d961ae4d97d395ecc9a03336b4376
christophedlr <christophedlr@gmail.com>
2020-09-28 16:20:17 +0200

    Add transaction
    - Add: Spacers
    - Add: comments
    - Add: close to click cancel button

--- manager/windows/QTransactionsAdd.py
+++ manager/windows/QTransactionsAdd.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import QDate
 from PyQt5.QtWidgets import QWidget, QPushButton, QComboBox, QTextEdit, QDateEdit, QDoubleSpinBox, QHBoxLayout, \
-    QVBoxLayout, QLabel
+    QVBoxLayout, QLabel, QSpacerItem
 
 
 class QTransactionsAdd(QWidget):
@@ -10,6 +10,7 @@ class QTransactionsAdd(QWidget):
         self.setWindowTitle("Add a transaction")
         self.resize(950, 450)
 
+        # QLabels
         label_date = QLabel("Date of transaction :")
         label_currency = QLabel("Currency :")
         label_type = QLabel("Type of transaction :")
@@ -19,6 +20,7 @@ class QTransactionsAdd(QWidget):
         date = QDateEdit(QDate.currentDate())
         date.setDisplayFormat("dd/MM/yyyy")
 
+        # Currencies combobox
         currency = QComboBox()
         currency.addItem("BTC")
         currency.addItem("ETH")
@@ -40,45 +42,69 @@ class QTransactionsAdd(QWidget):
         amount = QDoubleSpinBox()
         amount.setValue(0)
 
+        spacer_describe = QSpacerItem(1, 15)
         describe = QTextEdit()
 
         validate_button = QPushButton("Validate")
         cancel_button = QPushButton("Cancel")
 
+        # Vertical layout for date
         vlayout_date = QVBoxLayout()
         vlayout_date.addWidget(label_date)
         vlayout_date.addWidget(date)
 
+        # Vertical layout for currencies
         vlayout_currency = QVBoxLayout()
         vlayout_currency.addWidget(label_currency)
         vlayout_currency.addWidget(currency)
 
+        # Vertical layout for type of transaction
         vlayout_type = QVBoxLayout()
         vlayout_type.addWidget(label_type)
         vlayout_type.addWidget(type_transaction)
 
+        # Vertical layout for amount
         vlayout_amount = QVBoxLayout()
         vlayout_amount.addWidget(label_amount)
         vlayout_amount.addWidget(amount)
 
+        # Horizontal layout for transaction form
         hlayout_transaction = QHBoxLayout()
         hlayout_transaction.addLayout(vlayout_date)
         hlayout_transaction.addLayout(vlayout_currency)
         hlayout_transaction.addLayout(vlayout_type)
         hlayout_transaction.addLayout(vlayout_amount)
 
+        # Vertical layout for describe
         vlayout_describe = QVBoxLayout()
+        vlayout_describe.addSpacerItem(spacer_describe)
         vlayout_describe.addWidget(label_describe)
         vlayout_describe.addWidget(describe)
 
+        spacer_desc_buttons = QSpacerItem(1, 180)
+
+        # Vertical layout for buttons
         vlayout_buttons = QVBoxLayout()
         vlayout_buttons.addWidget(validate_button)
         vlayout_buttons.addWidget(cancel_button)
+        vlayout_buttons.addSpacerItem(spacer_desc_buttons)
 
+        spacer_desc_buttons = QSpacerItem(30, 1)
+
+        # Horizontal layout for describe & buttons
         hlayout_desc_buttons = QHBoxLayout()
         hlayout_desc_buttons.addLayout(vlayout_describe)
+        hlayout_desc_buttons.addSpacerItem(spacer_desc_buttons)
         hlayout_desc_buttons.addLayout(vlayout_buttons)
+        hlayout_desc_buttons.addSpacerItem(spacer_desc_buttons)
 
+        # Vertical layout for transaction layout & describe & buttons
         vertical_layout = QVBoxLayout(self)
         vertical_layout.addLayout(hlayout_transaction)
         vertical_layout.addLayout(hlayout_desc_buttons)
+
+        # Add signal event in clicked cancel button
+        cancel_button.clicked.connect(self.on_clicked_cancel_button)
+
+    def on_clicked_cancel_button(self):
+        self.close()
