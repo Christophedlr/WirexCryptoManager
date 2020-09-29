401345256eaab113e08db92d102790554acd8a3b
christophedlr <christophedlr@gmail.com>
2020-09-29 09:23:48 +0200

    SQLAlchemy
    - Update: config.json (adding type of database)
    - Add: TransactionsEntity import
    - Add: import INTEGER for sqlite or mysql for create unsigned_int variable
    - Add: config.json (database name and path)
    - Add: init-db CLI command for create datbase
    - SQLAchemy configuration
    
    Types of database: sqlite or mysql only (actually sqlite only)

--- __init__.py
+++ __init__.py
@@ -1,4 +1,15 @@
+import sys
 from manager import app
+from manager.common.sqlalchemy import engine, Base
+from manager.entities import TransactionsEntity
+
+
+def init_db():
+    Base.metadata.create_all(engine)
+
 
 if __name__ == "__main__":
-    app.exec_()
+    if sys.argv[1] == "init-db":
+        init_db()
+    else:
+        app.exec_()
