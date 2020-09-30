368ceae26b25c4e7360c842bb482289cd5f11fab
christophedlr <christophedlr@gmail.com>
2020-09-30 16:43:22 +0200

    Details of selected transaction
    - Add: QSpacerItem
    - Add: close to click in button
    
    WARNING! Click button don't work
    WARNING! Window is blank after reopen

--- manager/windows/QTransactionDetails.py
+++ manager/windows/QTransactionDetails.py
@@ -1,8 +1,6 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QSpacerItem
 from PyQt5.QtGui import QShowEvent
 from manager.entities.TransactionsEntity import TransactionsEntity
-from sqlalchemy.orm import sessionmaker
-from manager.common.sqlalchemy import engine
 
 
 class QTransactionDetails(QWidget):
@@ -14,6 +12,7 @@ class QTransactionDetails(QWidget):
     usd: QLabel
     describe: QLabel
     entity: TransactionsEntity
+    close_button: QPushButton
 
     def __init__(self):
         super().__init__()
@@ -35,7 +34,9 @@ class QTransactionDetails(QWidget):
         self.btc = QLabel()
         self.eur = QLabel()
         self.usd = QLabel()
+
         self.describe = QLabel()
+        self.describe.setWordWrap(True)
 
         vbox_date = QVBoxLayout()
         vbox_date.addWidget(label_date)
@@ -65,10 +66,10 @@ class QTransactionDetails(QWidget):
         vbox_describe.addWidget(label_describe)
         vbox_describe.addWidget(self.describe)
 
-        close_button = QPushButton("Close")
+        self.close_button = QPushButton("Close")
         hbox_desc_close = QHBoxLayout()
         hbox_desc_close.addLayout(vbox_describe)
-        hbox_desc_close.addWidget(close_button)
+        hbox_desc_close.addWidget(self.close_button)
 
         hbox = QHBoxLayout()
         hbox.addLayout(vbox_date)
@@ -80,7 +81,10 @@ class QTransactionDetails(QWidget):
 
         vbox = QVBoxLayout(self)
         vbox.addLayout(hbox)
+        vbox.addSpacerItem(QSpacerItem(0, 100))
         vbox.addLayout(hbox_desc_close)
+
+        self.close_button.clicked.connect(self.on_clicked_close_button)
 
     def showEvent(self, a0: QShowEvent) -> None:
         super().__init__()
@@ -92,12 +96,12 @@ class QTransactionDetails(QWidget):
             self.transaction.setText("Debits")
 
         amount = self.entity.amount
-        self.amount.setText("{:0.8f}".format(amount))
+        self.amount.setText("{:0.8f}".format(amount) + " " + self.entity.currency)
 
         if amount < 0:
-            self.amount.setStyleSheet("QLabel { color : red }")
+            self.amount.setStyleSheet('QLabel { color : red; }')
         else:
-            self.amount.setStyleSheet("QLabel { color : green }")
+            self.amount.setStyleSheet('QLabel { color : green; }')
 
         self.btc.setText("{:0.8f}".format(self.entity.btc))
         self.eur.setText("{:0.2f}".format(self.entity.eur))
@@ -106,3 +110,6 @@ class QTransactionDetails(QWidget):
 
     def setEntity(self, entity: TransactionsEntity):
         self.entity = entity
+
+    def on_clicked_close_button(self):
+        self.close()
